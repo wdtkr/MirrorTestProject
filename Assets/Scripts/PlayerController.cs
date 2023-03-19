19c7511ca09b83417b98dac478c5be581cb83c63
wdtkr <wdtkru@icloud.com>
2023-03-20 03:01:06 +0900

    とりあえず接続可能

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody rb;
+    void FixedUpdate()
     {
-        
+        // ローカルプレイヤーの時
+        if (isLocalPlayer) {
+            // 操作
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            if (Input.GetKeyDown(KeyCode.Q)) rb.velocity = Vector3.zero;
+            CmdMoveSphere(x, z);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    // NetworkBehaviourの「Cmd」ではじまるメソッドに[Command]タグを付加することで、
+    // クライアントから呼ばれた時、サーバー上で実行されるメソッドとして指定できる
+    // https://note.com/npaka/n/na2c1e7e1d53b#:~:text=%E3%82%AF%E3%83%A9%E3%82%A4%E3%82%A2%E3%83%B3%E3%83%88%E3%81%8B%E3%81%A9%E3%81%86%E3%81%8B-,%E2%97%8E%C2%A0%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89,-NetworkBehaviour%E3%81%AE%E3%80%8C
+    [Command]
+    void CmdMoveSphere(float x, float z) 
     {
-        
+        Vector3 v = new Vector3(x, 0, z) * 5f;
+        GetComponent<Rigidbody>().AddForce(v);
     }
 }
